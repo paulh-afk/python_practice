dbeac9d32318599e9a542bbab844e9e4371220ba
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-06-21 23:01:50 +0200

    def toCSV

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-def multiply(a, b):
-    return a * b
+def toCSV(liste):
+    csv = ""
+    for i in range(len(liste)):
+        values = liste[i].values()
+
+        for j in range(len(values)):
+            if(j != 0):
+                csv += ","
+            csv += list(values)[j]
+
+        if (i != (len(liste) - 1)):
+            csv += "\n"
+    return csv
 
 
-def total(account):
-    sum = 0
-    for money in account:
-        sum += money
-    return sum
+my_liste = [{"name": "Paul", "age": "12"}, {
+    "name": "Josh", "age": "14"}, {"name": "Daniel", "age": "22"}]
 
-
-result = multiply(-3, 9)
-print(result)
-
-my_account = [50.50, 25, -40, 150]
-my_money = total(my_account)
-print(my_money)
+csv_content = toCSV(my_liste)
+print(csv_content)
