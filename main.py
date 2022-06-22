7a0d08b9488dd7d30abc118b42c669ae7cb918bd
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-06-22 18:56:25 +0200

    def toCSV header

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 def toCSV(liste):
     csv = ""
     for i in range(len(liste)):
-        values = liste[i].values()
+        values = list(liste[i].items())
+
+        if(i == 0):
+            for k in range(len(values)):
+                csv += values[k][0]
+                if (k != (len(values) - 1)):
+                    csv += ","
+            csv += "\n"
 
         for j in range(len(values)):
             if(j != 0):
                 csv += ","
-            csv += list(values)[j]
+            csv += str(list(values)[j][1])
 
         if (i != (len(liste) - 1)):
             csv += "\n"
     return csv
 
 
-my_liste = [{"name": "Paul", "age": "12"}, {
-    "name": "Josh", "age": "14"}, {"name": "Daniel", "age": "22"}]
+my_liste = [{"name": "Paul", "age": "12", "isAdult": False}, {
+    "name": "Josh", "age": "14", "isAdult": False}, {"name": "Daniel", "age": "22", "isAdult": True}]
 
 csv_content = toCSV(my_liste)
 print(csv_content)
