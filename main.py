9e97fc65ea71d7cf887014006cda02c448bebd15
Paul <paul.helleu@orange.fr>
2022-07-10 16:49:55 +0200

    extract titles

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ url = "https://www.gov.uk/search/news-and-communications"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
-print(page.content)
+titles = soup.find_all(
+    "a", "gem-c-document-list__item-title")
+
+for title in titles:
+    print(title.string)
