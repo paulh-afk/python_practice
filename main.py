76f20d649cb22800a9a72bbac2f6fd119964d67e
Paul <paul.helleu@orange.fr>
2022-07-11 20:31:55 +0200

    etl title description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from array import array
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = "https://www.gov.uk/search/news-and-communications"
 page = requests.get(url)
@@ -21,5 +21,11 @@ descriptions = []
 for description in descriptions_bs:
     descriptions.append(description.string)
 
-print(titles)
-print(descriptions)
+head = ["titre", "description"]
+
+with open("data.csv", "w") as file:
+    writer = csv.writer(file, delimiter=",")
+    writer.writerow(head)
+
+    for title, description in zip(titles, descriptions):
+        writer.writerow([title, description])
