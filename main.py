09d37cc7208cecc189e94a16e2ea586d17e507c1
Paul <paul.helleu@orange.fr>
2022-07-11 17:20:41 +0200

    extract descriptions

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ soup = BeautifulSoup(page.content, "html.parser")
 titles = soup.find_all(
     "a", "gem-c-document-list__item-title")
 
-for title in titles:
-    print(title.string)
+descriptions = soup.find_all("p", "gem-c-document-list__item-description")
+
+# for title in titles:
+# print(title.string)
+
+for desc in descriptions:
+    print(desc.string)
