fb0ab6bfd605ee5e0c0ec78f983aa76e7e09ec22
Paul <paul.helleu@orange.fr>
2022-07-11 18:45:48 +0200

    storing data in array

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from array import array
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,13 +6,20 @@ url = "https://www.gov.uk/search/news-and-communications"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
-titles = soup.find_all(
+titles_bs = soup.find_all(
     "a", "gem-c-document-list__item-title")
 
-descriptions = soup.find_all("p", "gem-c-document-list__item-description")
+descriptions_bs = soup.find_all("p", "gem-c-document-list__item-description")
 
-# for title in titles:
-# print(title.string)
+titles = []
 
-for desc in descriptions:
-    print(desc.string)
+for title in titles_bs:
+    titles.append(title.string)
+
+descriptions = []
+
+for description in descriptions_bs:
+    descriptions.append(description.string)
+
+print(titles)
+print(descriptions)
